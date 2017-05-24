23259797c0dd3e9c719c5b0cf1faabdce79a1876
bgulbis <brian.gulbis@gmail.com>
2017-05-24 14:05:44 -0500

    move data to s3

--- src/02_move-data.R
+++ src/02_move-data.R
@@ -9,13 +9,31 @@ set_config(config(ssl_verifypeer = 0L))
 dir_raw <- "data/raw"
 bucket <- "iv-iron-mue"
 
-read_data(dir_raw, "demographics", FALSE) %>%
-    # write_rds("data/external/demographics.Rds", "gz")
-    s3saveRDS(object = "data/raw/demographics2.Rds",
-              bucket = bucket,
-              headers = list("x-amz-server-side-encryption" = "AES256"))
+move_data <- function(x) {
+    read_data(dir_raw, x, FALSE) %>%
+        s3saveRDS(object = paste0("data/raw/", x, ".Rds"),
+                  bucket = bucket,
+                  headers = list("x-amz-server-side-encryption" = "AES256"))
+}
+
+# mbo/edw data -----------------------------------------
+move_data("blood")
+move_data("demographics")
+move_data("diagnosis")
+move_data("labs")
+move_data("measures")
+move_data("meds")
+move_data("patients")
+move_data("vent")
+move_data("vitals")
+
+# vizient data -----------------------------------------
+put_object(file = "data/external/vizient_totals_peer-hospitals_2016.xlsx",
+           object = "data/external/vizient_totals.xlsx",
+           bucket = bucket,
+           headers = list("x-amz-server-side-encryption" = "AES256"))
 
 put_object(file = "data/external/vizient_service-line_peer-hospitals_2016.xlsx",
-           object = "data/external/vizient.xlsx",
+           object = "data/external/vizient_service-line.xlsx",
            bucket = bucket,
            headers = list("x-amz-server-side-encryption" = "AES256"))
